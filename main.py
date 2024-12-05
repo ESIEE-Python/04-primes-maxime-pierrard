c711a76c6b414a29618142d723e70acfcc1daff5
maxime-pierrard <maxime.pierrard@edu.esiee.fr>
2024-12-05 10:07:55 +0000

    Exo Finit

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-from math import sqrt
+"""
+Bonjour
+"""
+# Module pour déterminer si un entier est un nombre premier.
 
-#### Fonction secondaire
-
+#### Fonction secondaire ####
 
 def isprime(p):
+    """
+    Vérifie si un entier est un nombre premier.
 
-    # votre code ici
+    Cette fonction détermine si le nombre passé en paramètre est un
+    nombre premier ou non, en renvoyant un booléen.
 
-    pass
+    Args:
+        p (int): Le nombre à vérifier.
 
-#### Fonction principale
+    Returns:
+        bool: True si le nombre est premier, False sinon.
+    """
+    if p <= 1:
+        return False
+    for i in range(2, p - 1):
+        if p % i == 0:
+            return False
+    return True
 
+#### Fonction principale ####
 
 def main():
+    """
+    Point d'entrée principal du programme.
 
-    # vos appels à la fonction secondaire ici
-
+    Cette fonction utilise la fonction `isprime` pour afficher tous les
+    nombres premiers entre 0 et 99.
+    """
     for n in range(100):
         if isprime(n):
-            print(n, end=", ")
-
+            print(n, end=",")
     print()
-
 
 if __name__ == "__main__":
     main()
